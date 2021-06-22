a27191fabb6b38c2829a39b0b1f03b558fc60eb5
StarNeit <koneits@outlook.com>
2021-06-22 13:21:50 +0200

    IStakingRewards

--- contracts/StakingRewardsFactory.sol
+++ contracts/StakingRewardsFactory.sol
@@ -467,3 +467,29 @@ contract ReentrancyGuard {
         require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
     }
 }
+
+// Inheritance
+interface IStakingRewards {
+    // Views
+    function lastTimeRewardApplicable() external view returns (uint256);
+
+    function rewardPerToken() external view returns (uint256);
+
+    function earned(address account) external view returns (uint256);
+
+    function getRewardForDuration() external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    // Mutative
+
+    function stake(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function getReward() external;
+
+    function exit() external;
+}
